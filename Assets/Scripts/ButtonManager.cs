6df0d2e0a9255e9b011aa5c62050fb74f8544202
Lukasz Gornik <lukasz.gornicki.1995@gmail.com>
2017-05-21 23:30:17 +0200

    Category and background

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,15 +6,36 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
 
+    public Canvas MainCanvas;
+    public Canvas CategoryCanvas;
+
+    void Awake()
+    {
+        CategoryCanvas.enabled = false;
+        MainCanvas.enabled = true;
+    }
+
     public void NewGameBtn()
     {
-        SceneManager.LoadScene("main");
+        MainCanvas.enabled = false;
+        CategoryCanvas.enabled = true;
     }
 
     public void ExitGameBtn()
     {
         Application.Quit();
     }
+
+    public void OnCategory1()
+    {
+        MainCanvas.enabled = false;
+        CategoryCanvas.enabled = false;
+
+        GameData.SET = "Animals";
+        GameData.BASE_SPRITE = "cat";
+        SceneManager.LoadScene("main");
+    }
+
 
 }
 
